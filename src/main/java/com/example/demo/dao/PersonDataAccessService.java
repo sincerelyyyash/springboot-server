8368ddfe9fe62163be2f11d3b32d6c336526153a
sincerelyyyash <yta0088@gmail.com>
2024-07-29 23:59:26 +0530

    Data service update

--- src/main/java/com/example/demo/dao/PersonDataAccessService.java
+++ src/main/java/com/example/demo/dao/PersonDataAccessService.java
@@ -1,6 +1,8 @@
 package com.example.demo.dao;
 
 import com.example.demo.model.Person;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -9,6 +11,15 @@ import java.util.UUID;
 
 @Repository("postgres")
 public class PersonDataAccessService implements PersonDao{
+
+    private final JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    public PersonDataAccessService(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+
     @Override
     public int insertPerson(UUID id, Person person) {
         return 0;
@@ -16,6 +27,11 @@ public class PersonDataAccessService implements PersonDao{
 
     @Override
     public List<Person> selectAllPeople() {
+        final String sql = "SELECT id, name FROM person";
+        jdbcTemplate.query(sql, (resulSet, i)->{
+            return Person(UUID.fromString(resulSet.getString("id")),
+                    resulSet.getString("name"))
+        });
         return List.of(new Person(UUID.randomUUID(), "From Postgres DB"));
     }
 
